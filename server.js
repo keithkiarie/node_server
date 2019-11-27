f4dc0a9c099c9f67754d382548670da7e3f686f5
Keithkiarie <keithkiarie@gmail.com>
2019-11-27 12:12:36 +0300

    serve files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+let folder_name = 'public'; //folder that holds static resources to be served
+
+app.use(express.static(path.join(__dirname, folder_name)));
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT => console.log(`Server started on port ${PORT}`));
